74110c73d2b183f05d5103be9185d4a7fcb1ea9e
kaengkaeng <167205218+kaengkaeng@users.noreply.github.com>
2024-06-11 13:37:06 +0200

    OOP

---
+++ kaffee.scala
@@ -0,0 +1,22 @@
+class Kaffeautomat(private val preis :Int): // preis ist primaere konstruktor
+  private var summe:Int = 0 // 현재 금액 (this)
+
+  // 이 클래스는 커피 preis를 받아 초기화한다.
+
+  def einwerfen(muenze: Int): Unit =
+    if (muenze == 1 || muenze == 2 || muenze == 5) then
+        this.summe += muenze
+        println(this.summe)
+    else
+        println("Falsche Muenze")
+
+  def mehr_einwerfen(muenzen:List[Int]):Unit =
+    for (x <- muenzen) do
+      einwerfen(x) // 위의 함수를 호출해서 값을 더한 값을 출력
+
+  def kochen():Unit =
+    if (this.summe >= preis)
+      val restgeld = this.summe - preis
+      println (restgeld)
+    else
+      println("nicht genuegend Geld")
