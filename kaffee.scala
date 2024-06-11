b358c2f2f913146fb8289d8eeb82b23b2aaf08e4
kaengkaeng <167205218+kaengkaeng@users.noreply.github.com>
2024-06-11 13:39:40 +0200

    kaffee.scala

--- kaffee.scala
+++ kaffee.scala
@@ -1,11 +1,12 @@
-class Kaffeautomat(private val preis :Int): // preis ist primaere konstruktor
-  private var summe:Int = 0 // 현재 금액 (this)
+class Kaffeautomat(private val preis :Int): 
+  private var summe:Int = 0
 
+   // preis, summe ist primaere konstruktor
   // 이 클래스는 커피 preis를 받아 초기화한다.
 
   def einwerfen(muenze: Int): Unit =
     if (muenze == 1 || muenze == 2 || muenze == 5) then
-        this.summe += muenze
+        this.summe += muenze // sekundaere konstruktor 
         println(this.summe)
     else
         println("Falsche Muenze")
